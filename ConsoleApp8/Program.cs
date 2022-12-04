2231d72f639239b1b9ee9a2649d113e4f820094f
Rafet <rafetr@risk.az>
2022-12-04 14:31:33 +0400

    wef

--- ConsoleApp8/Program.cs
+++ ConsoleApp8/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World! masterr");
+            Console.WriteLine("Hello, World! ghjbnkjdnka");
         }
     }
 }
